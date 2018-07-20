957f6e817491fa2b016dcb145599ee386729c919
Thatiane Pisco <thatianepisco@gmail.com>
2018-07-20 14:40:19 +0100

    fix clean folder path

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
-var http = require('http')
-    ,app = require('./config/express'),
+var http = require('http'),
+    app = require('./config/express'),
     utils = require('./app/utils'),
+    path =  require('path'),
     root = process.cwd();
 
 http.createServer(app).listen((process.env.PORT || 3000), function() {
     console.log('Server running on port: ' + this.address().port);
-    setInterval(() => { utils.cleanFolder('uploads', 60);}, 60 * 1000);
+    setInterval(() => { utils.cleanFolder(path.join(__dirname, '/public/uploads'), 60);}, 60 * 1000);
 });
