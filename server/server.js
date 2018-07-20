d9be28c702c641ddd41371056c5b6265e54b727f
Thatiane Pisco <thatianepisco@gmail.com>
2018-07-20 15:35:01 +0100

    change clean folder interval time

--- server/server.js
+++ server/server.js
@@ -6,5 +6,5 @@ var http = require('http'),
 
 http.createServer(app).listen((process.env.PORT || 3000), function() {
     console.log('Server running on port: ' + this.address().port);
-    setInterval(() => { utils.cleanFolder(path.join(__dirname, '/public/uploads'), 60);}, 60 * 1000);
+    setInterval(() => { utils.cleanFolder(path.join(__dirname, '/public/uploads'), 60 * 60);}, 60 * 60 * 1000);
 });
