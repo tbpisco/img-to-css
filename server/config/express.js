0166b48de234fb449ccdbf5e81d6ef9ebf09391d
Thatiane Pisco <thatianepisco@gmail.com>
2018-07-20 14:15:06 +0100

    change express configurations

--- server/config/express.js
+++ server/config/express.js
@@ -4,26 +4,34 @@ var express = require('express')
     ,path =  require('path')
     ,bodyParser = require('body-parser')
     ,cors = require('cors'),
-    compileSass = require('express-compile-sass'),
-    root = process.cwd();
+    compileSass = require('express-compile-sass');
 
 app.use(cors());
 
 app.use(compileSass({
-    root: root,
+    root: path.join(__dirname, '/../../server', 'public'),
     sourceMap: false, 
     sourceComments: false, 
     watchFiles: false, 
     logToConsole: true
 }));
 
-app.use(express.static(root));
+app.use(compileSass({
+    root: path.join(__dirname, '/../../server/public', 'uploads'),
+    sourceMap: false, 
+    sourceComments: false, 
+    watchFiles: false, 
+    logToConsole: true
+}));
+
     
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/../../client', 'build', 'index.html')); 
 });
     
 app.use(express.static(path.join(__dirname, '/../../client', 'build'))); 
+app.use(express.static(path.join(__dirname, '/../../server', 'public'))); 
+app.use(express.static(path.join(__dirname, '/../../server/public', 'uploads'))); 
 
 app.use(express.json({limit: '5000000mb'}));
 app.use(express.urlencoded({extended: true, limit: '5000000mb'}));
